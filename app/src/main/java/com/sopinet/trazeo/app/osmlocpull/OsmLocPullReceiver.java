ea11bb14036ccb48f752b7e3db231a229abdc9df
hidabe <fhidalgo@sopinet.com>
2014-04-28 17:17:01 +0200

    begin notification system

--- app/src/main/java/com/sopinet/trazeo/app/osmlocpull/OsmLocPullReceiver.java
+++ app/src/main/java/com/sopinet/trazeo/app/osmlocpull/OsmLocPullReceiver.java
@@ -1,6 +1,7 @@
 package com.sopinet.trazeo.app.osmlocpull;
 
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,10 +11,14 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.AlarmClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.sopinet.android.nethelper.SimpleContent;
+import com.sopinet.trazeo.app.MonitorActivity;
+import com.sopinet.trazeo.app.MonitorActivity_;
 import com.sopinet.trazeo.app.helpers.MyLoc;
 import com.sopinet.trazeo.app.helpers.Var;
 
@@ -26,6 +31,8 @@ public class OsmLocPullReceiver extends BroadcastReceiver {
     // public MyLoc myLoc;
     public String url;
     public String data;
+    public static NotificationManager notificationManager = null;
+    public static NotificationCompat.Builder mBuilder = null;
 
     public OsmLocPullReceiver() {
     }
@@ -65,7 +72,7 @@ public class OsmLocPullReceiver extends BroadcastReceiver {
             }
 
             if (this.url != null) {
-                SimpleContent sc = new SimpleContent(context, "trazeo", 3);
+                SimpleContent sc = new SimpleContent(context, "trazeo", 0);
                 this.data += "&latitude=" + lat;
                 this.data += "&longitude=" + lon;
                 Log.d("TEMA", "DATA_SENDPOINT: "+this.data);
@@ -78,6 +85,9 @@ public class OsmLocPullReceiver extends BroadcastReceiver {
                     e.printStackTrace();
                 }
                 Log.d("TEMA", result);
+
+
+//                mBuilder.setContentText(result);
             }
 
             //location.getLatitude();
@@ -86,13 +96,47 @@ public class OsmLocPullReceiver extends BroadcastReceiver {
 
 
         // Put here YOUR code.
-        Toast.makeText(context, "Alarm! " + lat + " - " + lon, Toast.LENGTH_LONG).show(); // For example
+        //Toast.makeText(context, "Alarm! " + lat + " - " + lon, Toast.LENGTH_LONG).show(); // For example
 
         wl.release();
     }
 
     public void SetLocPull(Context context)
     {
+        mBuilder = new NotificationCompat.Builder(context);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        /*
+        dynamic class
+
+        		Class<?> cls = null;
+		try {
+			cls = Class.forName(MediaUploader.SENDINGCLASS);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+         */
+        // Estas clases podrían ser dinámicas
+        stackBuilder.addParentStack(MonitorActivity_.class);
+        Intent intent = new Intent(context, MonitorActivity_.class);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        mBuilder.setContentTitle("Inicializando...");
+        mBuilder.setSmallIcon(android.support.v7.appcompat.R.drawable.abc_ab_bottom_solid_dark_holo);
+        mBuilder.setNumber(12);
+        mBuilder.setProgress(100, 12, false);
+        mBuilder.setContentText("Empezando con el temita...");
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // Debe haber un ID, el "id_raid" estaría bien
+        notificationManager.notify(200, mBuilder.build());
+
+
         AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         //OsmLocPullReceiver osmLoc = new OsmLocPullReceiver(this.url, this.data);
         Intent i = new Intent(context, OsmLocPullReceiver.class);
